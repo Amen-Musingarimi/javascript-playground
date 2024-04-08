07fc95b7c9f221fa9629e2108c767cf1472abd2c
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-08 13:49:44 +0200

    Add type coercion exampe

--- basics/script.js
+++ basics/script.js
@@ -1,5 +1,28 @@
+/**************************************
+ * Variables and Data Types
+ **************************************/
+
+/*
 var firstName = 'Amen';
 console.log(firstName);
 
 var lastName = 'Musingarimi';
 var age = 29;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Sofware Developer';
+console.log(job);
+*/
+
+/**************************************
+ * Variable Mutation and Type Coercion
+ **************************************/
+const firstName = 'Amen';
+const age = 29;
+
+console.log(firstName + ' ' + age);
